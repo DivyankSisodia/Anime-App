d6e9ffd5a4209789957064bc1f04223599f02fb5
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-02-28 16:06:32 +0530

    Anime App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          // child: Text(
+          //   'Hello World!',
+          //   style: GoogleFonts.poppins(
+          //     fontSize: 24,
+          //     fontWeight: FontWeight.w800,
+          //   ),
+          // ),
+          child: Image.asset(
+            'assets/images/Home_image.jpg',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
