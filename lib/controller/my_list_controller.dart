f4eb304c10b3216303ab724597d4a21e7f985771
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-04 20:10:36 +0530

    added logic for the unwatched and download sections

--- lib/controller/my_list_controller.dart
+++ lib/controller/my_list_controller.dart
@@ -2,6 +2,8 @@ import 'package:anime_app/model/anime_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 List<Anime> watchedAnime = [];
+List<Anime> unwatchedAnime = [];
+List<Anime> downloadedAnime = [];
 
 class WatchedItemNotifier extends StateNotifier<List<Anime>> {
   WatchedItemNotifier() : super(watchedAnime);
@@ -22,4 +24,26 @@ class WatchedItemNotifier extends StateNotifier<List<Anime>> {
 
 final watchedAnimeProvider = StateNotifierProvider<WatchedItemNotifier, List<Anime>>((ref) {
   return WatchedItemNotifier();
+});
+
+class UnWatchedItemNotifier extends StateNotifier<List<Anime>> {
+  UnWatchedItemNotifier() : super(watchedAnime);
+
+  // add Anime to the list
+  void addToUnWatched(Anime anime) {
+    state = [...state, anime];
+  }
+
+  // remove Anime from the list
+  void removeFromUnWatched(String id) {
+    state = [
+      for (final anime in state)
+        if (anime.id != id) anime
+    ];
+  }
+}
+
+final unWatchedAnimeProvider =
+    StateNotifierProvider<UnWatchedItemNotifier, List<Anime>>((ref) {
+  return UnWatchedItemNotifier();
 });
