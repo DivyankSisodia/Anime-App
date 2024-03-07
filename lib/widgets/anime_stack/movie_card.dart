49c69d01ff781f2931c68fcf75ac6ff8b90d991a
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-07 17:48:45 +0530

    created movie section

--- lib/widgets/anime_stack/movie_card.dart
+++ lib/widgets/anime_stack/movie_card.dart
@@ -1,141 +1,18 @@
-// import 'dart:math';
-
-// import 'package:anime_app/controller/movie_data_controller.dart';
-// import 'package:anime_app/utils/constants/colors.dart';
-// import 'package:anime_app/utils/constants/style.dart';
-// import 'package:anime_app/widgets/HomeScreen/common/loading_effect.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-// import 'package:gap/gap.dart';
-
-// class MovieCard extends ConsumerWidget {
-//   const MovieCard({
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return Consumer(
-//       builder: (context, watch, child) {
-//         final movieList = ref.watch(movieDataProvider);
-//         return movieList.when(
-//           data: (moviesData) {
-//             return MasonryGridView.builder(
-//               physics: const NeverScrollableScrollPhysics(),
-//               itemCount: 10,
-//               shrinkWrap: true,
-//               gridDelegate:
-//                   const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 2,
-//               ),
-//               itemBuilder: (context, index) => Container(
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//                 height: 270,
-//                 child: Container(
-//                   decoration: BoxDecoration(
-//                     color: Colors.blueAccent,
-//                     borderRadius: BorderRadius.circular(10),
-//                     boxShadow: [
-//                       BoxShadow(
-//                         color: Colors.black.withOpacity(0.1),
-//                         blurRadius: 8,
-//                         offset: const Offset(0, 6),
-//                         spreadRadius: 2,
-//                       ),
-//                     ],
-//                   ),
-//                   margin: const EdgeInsets.all(12),
-//                   width: MediaQuery.of(context).size.width * 0.5,
-//                   child: Column(
-//                     children: [
-//                       Expanded(
-//                         child: Container(
-//                           margin: const EdgeInsets.all(12),
-//                           color: Colors.amber,
-//                           child: Image.asset(
-//                             moviesData[index].poster,
-//                             width: double.infinity,
-//                             fit: BoxFit.cover,
-//                           ),
-//                         ),
-//                       ),
-//                       const Gap(4),
-//                       Container(
-//                         padding: const EdgeInsets.symmetric(horizontal: 14),
-//                         child: Column(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             Text(
-//                               moviesData[index].name,
-//                               style: DStyle.smallHeadingText,
-//                             ),
-//                             const Text(
-//                               'Subheading??',
-//                               style: DStyle.smallHeadingText,
-//                             ),
-//                             Row(
-//                               mainAxisAlignment: MainAxisAlignment.start,
-//                               crossAxisAlignment: CrossAxisAlignment.center,
-//                               children: [
-//                                 Expanded(
-//                                   // Add this Expanded widget
-//                                   child: Text(
-//                                     moviesData[index].duration,
-//                                     maxLines: 1,
-//                                     style: DStyle.smallHeadingText,
-//                                   ),
-//                                 ),
-//                                 IconButton(
-//                                   onPressed: () {},
-//                                   icon: const Icon(
-//                                     Icons.bookmark,
-//                                     color: Colors.greenAccent,
-//                                     size: 30, // Adjust the size as needed
-//                                   ),
-//                                 ),
-//                               ],
-//                             )
-//                           ],
-//                         ),
-//                       )
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             );
-//           },
-//           error: ((error, stackTrace) => Center(
-//                 child: Text(
-//                   'Error : $error',
-//                   style: DStyle.mediumHeading,
-//                 ),
-//               )),
-//           loading: () => const LoadingEffect(),
-//         );
-//       },
-//     );
-//   }
-// }
-
 import 'package:anime_app/utils/helper/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:gap/gap.dart';
 
-import '../../controller/movie_data_controller.dart';
+import '../../controller/category/movie_data_controller.dart';
 import '../../model/movie_model.dart';
 import '../../utils/constants/style.dart';
 import '../HomeScreen/common/loading_effect.dart';
 
 class MovieCard extends ConsumerWidget {
   const MovieCard({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -202,9 +79,8 @@ class MovieCard extends ConsumerWidget {
                                   style: DStyle.smallHeadingText,
                                 ),
                                 Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
                                     Expanded(
                                       // Add this Expanded widget
