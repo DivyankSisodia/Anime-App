53e090c774bfbd0264e2eabd196da512790550f6
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-08 22:17:11 +0530

    fetching data from firebase

---
+++ lib/services/Firebase/User_data.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserDataService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String?> fetchUsername() async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore
+          .collection('users')
+          .limit(1) // Fetch only the first document
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        DocumentSnapshot documentSnapshot = querySnapshot.docs.first;
+        return documentSnapshot.get('username');
+      }
+    } catch (e) {
+      print('Error fetching username: $e');
+    }
+    return null;
+  }
+}
