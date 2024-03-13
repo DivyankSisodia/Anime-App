a2dcbf9247849ad8a30fe9b2360f838a8f276414
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-13 21:41:07 +0530

    model for episode data

---
+++ lib/data/anime_pics.dart
@@ -0,0 +1,13 @@
+class AnimeItem {
+  final String episodeNum;
+  final String image;
+  final String descp;
+  final String title;
+
+  AnimeItem({
+    required this.episodeNum,
+    required this.image,
+    required this.descp,
+    required this.title,
+  });
+}
