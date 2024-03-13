22a3de5cb5fc1f6b51895992f6c8c2b59b1ca31a
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-13 21:40:13 +0530

    created static data for episodes list

---
+++ lib/model/static_data.dart
@@ -0,0 +1,32 @@
+import '../data/anime_pics.dart';
+
+final List epsiodes = [
+  AnimeItem(
+    episodeNum: '1',
+    image: 'assets/images/Action_fig4.jpeg',
+    title: 'Vagabond',
+    descp:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  ),
+  AnimeItem(
+    title: 'Luffy',
+    episodeNum: '2',
+    image: 'assets/images/splash_image.jpeg',
+    descp:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  ),
+  AnimeItem(
+    title: 'One Piece',
+    episodeNum: '3',
+    image: 'assets/images/One_Piece _characters_Anime_Poster.jpeg',
+    descp:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  ),
+  AnimeItem(
+    title: 'Tatakae',
+    episodeNum: '4',
+    image: 'assets/images/Home_image.jpg',
+    descp:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  ),
+];
