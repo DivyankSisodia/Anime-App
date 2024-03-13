e49a2c7ebf7faa711e1de0fe61deb84f936faa92
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-13 22:47:04 +0530

    Anime Movies

--- lib/widgets/anime_stack/movie_card.dart
+++ lib/widgets/anime_stack/movie_card.dart
@@ -6,8 +6,10 @@ import 'package:gap/gap.dart';
 
 import '../../controller/category/movie_data_controller.dart';
 import '../../model/movie_model.dart';
+
+import '../../screen/detail_page/movie_anime_detail_page.dart';
 import '../../utils/constants/style.dart';
-import '../HomeScreen/common/loading_effect.dart';
+import '../HomeScreen/common/vertical_loading_effect.dart';
 
 class MovieCard extends ConsumerWidget {
   const MovieCard({
@@ -31,80 +33,112 @@ class MovieCard extends ConsumerWidget {
                     const SliverSimpleGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                 ),
-                itemBuilder: (context, index) => Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  height: DHelperFunctions.screenHeight(context) * 0.35,
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: const Duration(milliseconds: 900),
+                        reverseTransitionDuration:
+                            const Duration(milliseconds: 600),
+                        opaque: false,
+                        pageBuilder: (context, animation, secondaryanimation) {
+                          animation = CurvedAnimation(
+                            parent: animation,
+                            curve: Curves.decelerate,
+                          );
+                          return FadeTransition(
+                            opacity: animation,
+                            child: MovieDetailScreen(
+                              movie: moviesData[index],
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Colors.blueAccent,
                       borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 8,
-                          offset: const Offset(0, 6),
-                          spreadRadius: 2,
-                        ),
-                      ],
                     ),
-                    margin: const EdgeInsets.all(12),
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: const EdgeInsets.all(12),
-                            color: Colors.amber,
-                            child: Image.network(
-                              moviesData[index].poster,
-                              width: double.infinity,
-                              fit: BoxFit.cover,
+                    height: DHelperFunctions.screenHeight(context) * 0.35,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 8,
+                            offset: const Offset(0, 6),
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      margin: const EdgeInsets.all(12),
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.all(12),
+                              color: Colors.amber,
+                              child: Image.network(
+                                moviesData[index].poster,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                        ),
-                        const Gap(4),
-                        Flexible(
-                          child: Container(
-                            height: 200,
-                            padding: const EdgeInsets.symmetric(horizontal: 14),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Text(
-                                  moviesData[index].name,
-                                  maxLines: 2,
-                                  style: DStyle.smallHeadingText,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Expanded(
-                                      // Add this Expanded widget
-                                      child: Text(
-                                        'Length : ${moviesData[index].duration}',
-                                        maxLines: 1,
-                                        style: DStyle.smallHeadingText,
-                                      ),
+                          const Gap(4),
+                          Flexible(
+                            child: SizedBox(
+                              height: 200,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 11,
                                     ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                        Icons.bookmark,
-                                        color: Colors.greenAccent,
-                                        size: 30, // Adjust the size as needed
-                                      ),
+                                    child: Text(
+                                      moviesData[index].name,
+                                      maxLines: 3,
+                                      style: DStyle.smallHeadingText,
                                     ),
-                                  ],
-                                )
-                              ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Duration : ${moviesData[index].duration}',
+                                          maxLines: 1,
+                                          style: DStyle.smallHeadingText,
+                                        ),
+                                        Flexible(
+                                          child: IconButton(
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                              Icons.bookmark,
+                                              color: Colors.greenAccent,
+                                              size:
+                                                  20, // Adjust the size as needed
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -116,7 +150,7 @@ class MovieCard extends ConsumerWidget {
                     style: DStyle.mediumHeading,
                   ),
                 )),
-            loading: () => const LoadingEffect(),
+            loading: () => const VerticalLoadingEffect(),
           );
         },
       ),
