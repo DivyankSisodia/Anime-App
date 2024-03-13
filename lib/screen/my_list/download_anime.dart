85fab6ca7bff4ba0d77cb98e8ab968d337ac4499
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-13 21:50:29 +0530

    Downloaded Anime Screen

--- lib/screen/my_list/download_anime.dart
+++ lib/screen/my_list/download_anime.dart
@@ -34,75 +34,76 @@ class DownloadAnimePage extends ConsumerWidget {
                   child: Card(
                     color: DColors.backgroundColor,
                     child: Container(
-                        height: 150,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: DColors.searchTileColor,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: DColors.pureWhite,
-                              blurRadius: 5,
-                              spreadRadius: 0.5,
-                              offset: Offset(0, 0),
-                            )
-                          ],
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            CachedNetworkImage(
-                              imageUrl: anime.poster,
-                              height: 150,
-                              width: 150,
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: DColors.searchTileColor,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: DColors.pureWhite,
+                            blurRadius: 5,
+                            spreadRadius: 0.5,
+                            offset: Offset(0, 0),
+                          )
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl: anime.poster,
+                            height: 150,
+                            width: 150,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                      'Rank : ${anime.rank.toString()}',
+                                      style: DStyle.mediumHeading,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        ref
+                                            .read(downloadedAnimeProvider
+                                                .notifier)
+                                            .removeFromDownloaded(anime.id);
+                                      },
+                                      child: const Icon(
+                                        Icons.delete,
+                                        color: Colors.red,
+                                        size: 30,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Gap(5),
+                                Text(
+                                  anime.name,
+                                  maxLines: 1,
+                                  style: DStyle.lightbuttonText,
+                                ),
+                                const Gap(5),
+                                Text(
+                                  anime.description,
+                                  maxLines: 2,
+                                  style: DStyle.smalllightbuttonText,
+                                ),
+                              ],
                             ),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.5,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Text(
-                                        'Rank : ${anime.rank.toString()}',
-                                        style: DStyle.mediumHeading,
-                                      ),
-                                      GestureDetector(
-                                        onTap: () {
-                                          ref
-                                              .read(downloadedAnimeProvider
-                                                  .notifier)
-                                              .removeFromDownloaded(anime.id);
-                                        },
-                                        child: const Icon(
-                                          Icons.delete,
-                                          color: Colors.red,
-                                          size: 30,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const Gap(5),
-                                  Text(
-                                    anime.name,
-                                    maxLines: 1,
-                                    style: DStyle.lightbuttonText,
-                                  ),
-                                  const Gap(5),
-                                  Text(
-                                    anime.description,
-                                    maxLines: 2,
-                                    style: DStyle.smalllightbuttonText,
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        )),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 );
               },
